bc8432b2d4f9b8a6b506e3c9452777c16103748f
Lancelotti-beta <brunojosemarques.pe@gmail.com>
2022-05-19 14:42:02 -0300

    Iniciando repositório e trabalhando aritimética no REPL no NodeJS

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # js-parteI-aluraEscolaDeTecnologia
+
+## Aula de Programação com NodeJs 
+
+entedendo o basíco de Programação e ininciando com JavaScript
---
+++ meuPrograma.js
@@ -0,0 +1,10 @@
+console.log('Iniciando o Curso de JS para Front-End da Alura')
+
+const idade = 25;
+
+console.log('Idade: ' + idade);
+
+console.log(idade + 1)
+console.log(idade * 2)
+console.log(idade - 42)
+console.log(idade / 3)
